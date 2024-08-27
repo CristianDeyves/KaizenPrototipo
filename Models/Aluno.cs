8b8f949b8077176358f1019cdd2c81ce2121a109
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-27 16:00:05 -0300

    chore: Update Aluno class to include additional student information

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -7,27 +7,36 @@ using PrototipoKaizen.DTOs.Aluno;
 
 namespace PrototipoKaizen.Models
 {
-    public class Aluno
+    public class Aluno : Pessoa
     {
         public Guid Id { get; set; }
-        public string Nome { get; set; }
-        public string Email { get; set; }
+        public bool Ativo { get; set; }
         public DateTime DataNascimento { get; set; }
-        public string Telefone { get; set; }
-        public Endereco Endereco { get; set; }
+        public ContatoEmergencia ContatoEmergencia { get; set; }
+        public Responsavel Responsavel { get; set; }
+        public Cobranca Cobranca { get; set; }
+        public Mentor Mentor { get; set; }
+        public Guid TurmaId { get; set; }
+        public Turma Turma { get; set; }
+        public List<Aula> Aulas { get; set; }
+        public List<DiarioBordo> DiariosBordo { get; set; }
+        public string DiasAulasContratados { get; set; }
 
-        public Aluno(CreateAlunoDto novoAluno, Endereco endereco)
+        public Aluno(CreateAlunoDto novoAluno)
         {
             Id = Guid.NewGuid();
-            Nome = novoAluno.Nome;
-            Email = novoAluno.Email;
-
-            DataNascimento = DateTime.ParseExact(
-                novoAluno.DataNascimento, "dd/MM/yyyy", CultureInfo.InvariantCulture
-            );
-
-            Telefone = novoAluno.Telefone;
-            Endereco = endereco;
+            Ativo = novoAluno.Ativo;
+            Nome = novoAluno.pessoa.Nome;
+            Cpf = novoAluno.pessoa.Cpf;
+            Rg = novoAluno.pessoa.Rg;
+            DataNascimento = novoAluno.DataNascimento;
+            ContatoEmergencia = novoAluno.ContatoEmergencia;
+            Responsavel = novoAluno.Responsavel;
+            Cobranca = novoAluno.Cobranca;
+            Mentor = novoAluno.Mentor;
+            TurmaId = novoAluno.TurmaId;
+            Turma = novoAluno.Turma;
+            DiasAulasContratados = novoAluno.DiasAulasContratados;
         }
     }
 }
