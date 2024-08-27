578b078d7d48e062de3487d9875e39d093af3403
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-27 16:00:00 -0300

    chore: Add UpdateEmailDto for managing student email updates

---
+++ DTOs/Email/UpdateEmailDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrototipoKaizen.DTOs.Email
+{
+    public record UpdateEmailDto
+    (
+        string Email
+    );
+}
