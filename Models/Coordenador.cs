135ce19e311963f1a80348f20f7079c3cadbba0c
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-28 20:09:47 -0300

    feat: Add properties and relationships to Coordenador class

--- Models/Coordenador.cs
+++ Models/Coordenador.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace PrototipoKaizen.Models
 {
-    public class Coordenador
+    public class Coordenador : Pessoa
     {
-        
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public ContatoEmergencia ContatoEmergencia { get; set; }
+        public List<Mentor>? Mentores { get; set; }
+        public List<Turma>? Turmas { get; set; }
+        public List<Aula>? Aulas { get; set; }
+        public List<Formacoes>? FormacoesMinistradas { get; set; }
+        public List<Aviso>? Avisos { get; set; }
     }
 }
