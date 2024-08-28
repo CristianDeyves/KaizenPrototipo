056e4c9e7a719e78d3e0ebb5957a229f54557008
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-28 20:10:28 -0300

    feat: Add Mentor class with properties and relationships

--- Models/Mentor.cs
+++ Models/Mentor.cs
@@ -5,8 +5,18 @@ using System.Threading.Tasks;
 
 namespace PrototipoKaizen.Models
 {
-    public class Mentor
+    public class Mentor : Pessoa
     {
-        
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public ContatoEmergencia? ContatoEmergencia { get; set; }
+        public InformacoesPagamento? InformacoesPagamento { get; set; }
+        public List<Turma>? Turmas { get; set; }
+        public List<Aluno>? Alunos { get; set; }
+        public List<Aula>? Aulas { get; set; }
+        public List<DiarioBordo>? DiariosBordo { get; set; }
+        public List<Formacoes>? Formacoes { get; set; }
+        public string TipoMentor { get; set; }
     }
 }
