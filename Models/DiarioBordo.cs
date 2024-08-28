e14372bfa16bfa760c489ab301e1c44fe2b6a29b
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-28 20:10:09 -0300

    feat: Add properties and relationships to DiarioBordo class

--- Models/DiarioBordo.cs
+++ Models/DiarioBordo.cs
@@ -7,6 +7,11 @@ namespace PrototipoKaizen.Models
 {
     public class DiarioBordo
     {
-        
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public Aluno Aluno { get; set; }
+        public Mentor Mentor { get; set; }
+        public Aviso Aviso { get; set; }
+        public string ProximaAula { get; set; }
     }
 }
