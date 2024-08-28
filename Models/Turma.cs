d73c086a918b4a79639564ced8242a7fcd7c7c02
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-28 20:10:38 -0300

    feat: Add properties and relationships to Turma class

--- Models/Turma.cs
+++ Models/Turma.cs
@@ -7,6 +7,13 @@ namespace PrototipoKaizen.Models
 {
     public class Turma
     {
-        
+        public Guid Id { get; set; }
+        public bool Ativo { get; set; }
+        public string Codigo { get; set; }
+        public Guid MentorId { get; set; }
+        public Mentor Mentor { get; set; }
+        public Guid CoordenadorID { get; set; }
+        public Coordenador Coordenador { get; set; }
+        public List<Aluno>? Alunos { get; set; }
     }
 }
