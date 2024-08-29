d19985e9db76e0891d0031987c169bf135800669
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-29 17:45:03 -0300

    feat: Adicionar propriedades nulas Codigo, Mentor e Coordenador à classe Turma

--- Models/Turma.cs
+++ Models/Turma.cs
@@ -9,11 +9,11 @@ namespace PrototipoKaizen.Models
     {
         public Guid Id { get; set; }
         public bool Ativo { get; set; }
-        public string Codigo { get; set; }
+        public string? Codigo { get; set; }
         public Guid MentorId { get; set; }
-        public Mentor Mentor { get; set; }
+        public Mentor? Mentor { get; set; }
         public Guid CoordenadorID { get; set; }
-        public Coordenador Coordenador { get; set; }
+        public Coordenador? Coordenador { get; set; }
         public List<Aluno>? Alunos { get; set; }
     }
 }
