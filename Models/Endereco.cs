69c55464b0e029eb0e50aa828c0fe47881cd9093
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-29 17:44:25 -0300

    Adicionar propriedades nulas Endereco à classe Endereco

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -7,13 +7,13 @@ namespace PrototipoKaizen.Models
 {
     public class Endereco
     {
-        public string Cep { get; set; }
-        public string Logradouro { get; set; }
-        public string Numero { get; set; }
-        public string Complemento { get; set; }
-        public string Bairro { get; set; }
-        public string Localidade { get; set; }
-        public string Uf { get; set; }
-        public string Ddd { get; set; }
+        public string? Cep { get; set; }
+        public string? Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Localidade { get; set; }
+        public string? Uf { get; set; }
+        public string? Ddd { get; set; }
     }
 }
