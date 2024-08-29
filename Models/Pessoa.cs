e756567aa3f7ebf89f088a811c5911c8d5ad7d50
Deyves Oliveira <cristian_deyvinho@hotmail.com>
2024-08-29 17:44:45 -0300

    feat: Adicionar propriedades nulas PrimeiroNome, SegundoNome, Sobrenome, Cpf, Rg, Endereco, Email e Telefone à classe Pessoa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -8,14 +8,14 @@ namespace PrototipoKaizen.Models
     public class Pessoa
     {
         // Informações pessoais
-        public string PrimeiroNome { get; set; }
-        public string SegundoNome { get; set; }
-        public string Sobrenome { get; set; }
-        public string Cpf { get; set; }
-        public string Rg { get; set; }
+        public string? PrimeiroNome { get; set; }
+        public string? SegundoNome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? Cpf { get; set; }
+        public string? Rg { get; set; }
         // Informações de contato
-        public Endereco Endereco { get; set; }
-        public string Email { get; set; }
-        public string Telefone { get; set; }
+        public Endereco? Endereco { get; set; }
+        public string? Email { get; set; }
+        public string? Telefone { get; set; }
     }
 }
